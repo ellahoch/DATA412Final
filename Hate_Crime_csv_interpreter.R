c13476049dc249a6edb4ce83f821c3a286b17681
ellahoch <eh2149a@american.edu>
2023-10-16 14:44:41 -0400

    10.16.01R

--- Hate_Crime_csv_interpreter.R
+++ Hate_Crime_csv_interpreter.R
@@ -1,8 +1,35 @@
 library(tidyverse)
+library(janitor)
 
 read_csv("hate_crime.csv") %>%
   filter(data_year == "2021") ->
-  data
+  df
 
-data
+df <- df |> 
+  rename(state = state_name)
 
+df <- df |> 
+  group_by(state)
+count_of_crimes <- summarize(df, n = n())
+
+leg_comp <- read_csv("state_leg2021.csv", skip = 1)
+leg_comp <- clean_names(leg_comp)
+
+combined_df <- count_of_crimes |> 
+  left_join(leg_comp, by = "state")
+
+combined_df <- combined_df |> 
+  rename(hate_crime_count = n)
+
+combined_df <- combined_df |> 
+  mutate(total_prop = (senate_rep + house_rep)/total_seats)
+
+combined_df <- combined_df |> 
+  filter(!is.na(total_prop))
+
+ggplot(combined_df, aes(total_prop, hate_crime_count)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "State Legislature Composition and 2021 Hate Crime Count",
+       y = "Number of Hate Crimes Reported",
+       x = "Proportion of Republicans in State Legislature")
